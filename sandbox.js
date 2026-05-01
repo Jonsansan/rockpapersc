7bc4367fcec6432b4918093d99fc8780548c0040
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2026-05-01 12:52:01 -0400

    1.6.1

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,11 @@
-let playbutton = document.getElementById("play-bttn");
 let resultbutton = document.getElementById("wipe-result-bttn");
-let username = document.getElementById("username-input");
+let username = document.getElementById("usernamebox");
 
+let playbutton = document.getElementById("play-bttn");
+document.addEventListener("DOMcontentLoaded", function () {
+  askButton.addEventListener("Onclick", gol);
+});
 function gol() {
   document.getElementById("username-inputInput");
-  console.log("username-inputInput");
+  console.log("usernameboxInput");
 }
