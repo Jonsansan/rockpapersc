49e25a78180343ca792abf40b4de8945b72367dc
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2026-05-12 12:46:53 -0400

    update 1.7.6

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,13 @@
 let resultbutton = document.getElementById("wipe-result-bttn");
-let username = document.getElementById("usernamebox");
-
+let usernamebox = document.getElementById("username-box");
 let playbutton = document.getElementById("play-bttn");
-document.addEventListener("DOMcontentLoaded", function () {
-  askButton.addEventListener("Onclick", gol);
+let rockbutton = document.getElementById("rock-button");
+let paperbutton = document.getElementById("paper-button");
+let scissorsbutton = document.getElementById("scissors-button");
+let humanoutput = document.getElementById("output-human");
+let gptoutput = document.getElementById("output-gpt");
+
+playbutton.addEventListener("click", function () {
+  const name = usernamebox.value;
+  console.log(name);
 });
-function gol() {
-  document.getElementById("username-inputInput");
-  console.log("usernameboxInput");
-}
